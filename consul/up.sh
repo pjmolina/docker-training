3d5e2bef0ac470b74f317730d57288c54e373055
Pedro J. Molina <pjmolina@gmail.com>
2021-03-12 11:41:58 +0100

    modern consul

--- consul/up.sh
+++ consul/up.sh
@@ -1,1 +1,5 @@
-docker run -d -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h consul --name=consul progrium/consul -server -bootstrap -dc=hispalis -node=choco0
+## old one
+# docker run -d -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h consul --name=consul progrium/consul -server -bootstrap -dc=hispalis -node=choco0
+
+## devopment server
+docker run -d --name=dev-consul -p 8400:8400 -p 8500:8500 -p 8600:53/udp -e CONSUL_BIND_INTERFACE=eth0 consul
