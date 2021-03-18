a3ab8a10c371fb019680017e7fe3a2faf7e6f374
Pedro J. Molina <pjmolina@gmail.com>
2021-03-18 11:01:49 +0100

    update portainer start on windows

--- portainer/run.sh
+++ portainer/run.sh
@@ -1,1 +1,13 @@
-docker run --name portainer -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
+# linux
+docker run --restart=always --name portainer -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce
+
+
+# win 10
+## From  https://gist.github.com/SeanSobey/344edd228922ffd4266ae7d451421ab6
+docker volume create portainer_data
+#Auth
+docker run -d -p 9000:9000 --name portainer --restart=always -v portainer_data:/data portainer/portainer-ce -H tcp://host.docker.internal:2375
+#no auth
+## docker run -d -p 9000:9000 --name portainer --restart=always -v portainer_data:/data portainer/portainer-ce --no-auth -H tcp://host.docker.internal:2375
+
+
