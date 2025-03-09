b8a515606a9551fe3f062248e936b402cfc547c7
Pedro J. Molina <pjmolina@metadev.pro>
2025-03-09 12:38:12 +0100

    fix

--- consul/up.sh
+++ consul/up.sh
@@ -1,5 +1,7 @@
 ## old one
-# docker run -d -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h consul --name=consul progrium/consul -server -bootstrap -dc=hispalis -node=choco0
+# docker run -d -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h consul \
+#   --name=consul hashicorp/consul \
+#   -server -bootstrap -dc=hispalis -node=choco0 \
 
 ## devopment server
 docker run -d --name=consul \
